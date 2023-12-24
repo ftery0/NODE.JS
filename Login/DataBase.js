8c9b46bbb4ac956fe311b6521479cd0055004d6c
해준 <127281057+ftery0@users.noreply.github.com>
2023-12-24 16:00:14 +0100

    feat :: 로그인만 완성

--- Login/DataBase.js
+++ Login/DataBase.js
@@ -1,12 +1,10 @@
 module.exports = [
     {
-        id:1,
         username: "이해준",
         id: "test",
         password: "1234"
     },{
-        id:1,
-        username: "박규민",
+        username: "박규민",
         id: "park",
         password: "1234"
     },
