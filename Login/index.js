bcb5125b689ba42e93f15b93c598278983378f29
해준 <127281057+ftery0@users.noreply.github.com>
2023-12-24 13:28:40 +0100

    feat :: Login

---
+++ Login/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const {
+  login,
+  accesstoken,
+  refreshtoken,
+  loginSuccess,
+  logout,
+} = require("./controller");
+const app = express();
+dotenv.config();
+
+//기본 설정
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:3002",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+
+app.post("/login",login);
+app.get("/accesstoken",accesstoken);
+app.get("/refreshtoken",refreshtoken);
+app.get("/login/success",loginSuccess);
+app.post("/logout",logout);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server start ", PORT);
+});
