16792a70ef197b30fde0ac767b7739551aede464
해준 <127281057+ftery0@users.noreply.github.com>
2023-12-28 22:05:28 +0100

    test user DataBase

--- Login/DataBase.js
+++ Login/DataBase.js
@@ -3,6 +3,12 @@ module.exports = [
         "id": "newuser",
         "username": "New User",
         "password": "newuserpassword"
+      },
+      {
+        id:"test7388",
+        username: "박규민",
+        password: "1234"
+
       }
       
 ]
