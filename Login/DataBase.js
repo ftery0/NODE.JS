967962e1b23b5696dfad36b3715d9a29a97de9c7
해준 <127281057+ftery0@users.noreply.github.com>
2023-12-28 22:09:45 +0100

    test :: 유저추가

--- Login/DataBase.js
+++ Login/DataBase.js
@@ -4,9 +4,14 @@ module.exports = [
         "username": "New User",
         "password": "newuserpassword"
       },
-      {
+      { // 웹 계정
         id:"test7388",
         username: "박규민",
+        password: "1234"
+      },
+      { // 앱 계정
+        id:"Test88",
+        username: "전우진",
         password: "1234"
 
       }
