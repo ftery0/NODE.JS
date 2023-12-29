1772b419ca62549081ad7f746c837bd2235acb43
해준 <127281057+ftery0@users.noreply.github.com>
2023-12-29 11:50:26 +0100

    🧪

--- Login/index.js
+++ Login/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const mysql = require("mysql");
 const {
   login,
   profile,
